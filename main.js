967c6addb798a41821a461a975191069d54b1ba3
maxwellerman <120071506+maxwellerman@users.noreply.github.com>
2023-11-30 22:06:46 -0500

    Update main.js

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ export async function redirectToAuthCodeFlow(clientId) {
   const params = new URLSearchParams();
   params.append("client_id", clientId);
   params.append("response_type", "code");	
-  params.append("redirect_uri", "http://localhost:5173/callback");
+  params.append("redirect_uri", "https://maxwellerman.github.io/spotifyjukebox/");
   params.append("scope", "user-read-playback-state user-modify-playback-state");
   params.append("code_challenge_method", "S256");
   params.append("code_challenge", challenge);
